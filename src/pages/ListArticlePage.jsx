8f366ddef41c7ce3ecb871c252286a5b43185f2a
yusufbahtiarr <yusufbdev@gmail.com>
2025-05-19 23:15:37 +0700

    feat: add pagination for article

--- src/pages/ListArticlePage.jsx
+++ src/pages/ListArticlePage.jsx
@@ -8,25 +8,28 @@ function ListArticlePage() {
   const [data, setData] = useState([]);
   const { handleSubmit, register } = useForm();
   const [searchParams, setSearchParams] = useSearchParams();
+  const pageFromParams = parseInt(searchParams.get("page") || "1", 10);
+  const searchQuery = searchParams.get("search") || "";
+  const [count, setCount] = useState(0);
+  const totalPages = Math.ceil(count / 5);
 
   async function fetchArticles(limit = 5, page = 1, query = "") {
     const offset = (page - 1) * limit;
     const { data } = await axios.get("/data.json");
-
     const filtered = data.filter((item) =>
       item.title.toLowerCase().includes(query.toLowerCase())
     );
 
+    setCount(filtered.length);
     setData(filtered.slice(offset, offset + limit));
   }
 
   useEffect(() => {
-    const searchQuery = searchParams.get("search") || "";
-    fetchArticles(5, 1, searchQuery);
+    fetchArticles(5, pageFromParams, searchQuery);
   }, [searchParams]);
 
   function searchData(value) {
-    setSearchParams({ search: value.search });
+    setSearchParams({ search: value.search, page: 1 });
   }
 
   return (
@@ -79,14 +82,13 @@ function ListArticlePage() {
             {searchParams.get("search") && (
               <div className="mb-6 text-gray-600">
                 Hasil pencarian untuk "{searchParams.get("search")}" (
-                <span className="font-medium">{data.length}</span> hasil
-                ditemukan)
+                <span className="font-medium">{count}</span> hasil ditemukan)
               </div>
             )}
           </div>
-          {data.map((post) => (
+          {data.map((post, index) => (
             <div
-              key={post.slug}
+              key={index}
               className="border border-gray-300 rounded-2xl shadow-lg p-6 flex flex-row gap-6"
             >
               <div className="flex flex-col flex-1 min-h-[150px]">
@@ -119,6 +121,41 @@ function ListArticlePage() {
               </div>
             </div>
           ))}
+          {data.length > 0 && (
+            <div className="flex justify-center items-center gap-4 mt-6 text-sm w-full">
+              <button
+                disabled={pageFromParams === 1}
+                onClick={() =>
+                  setSearchParams({
+                    ...(searchQuery && { search: searchQuery }),
+                    page: pageFromParams - 1,
+                  })
+                }
+                className={`font-semibold size-[35px] rounded-full border cursor-pointer ${
+                  pageFromParams === 1 ? "opacity-50 cursor-none" : ""
+                }`}
+              >
+                {"<"}
+              </button>
+
+              <span className="px-4 py-2">Halaman {pageFromParams}</span>
+
+              <button
+                disabled={pageFromParams >= totalPages}
+                onClick={() =>
+                  setSearchParams({
+                    ...(searchQuery && { search: searchQuery }),
+                    page: pageFromParams + 1,
+                  })
+                }
+                className={` font-semibold size-[35px] rounded-full border cursor-pointer ${
+                  pageFromParams >= totalPages ? "opacity-50 cursor-none" : ""
+                }`}
+              >
+                {">"}
+              </button>
+            </div>
+          )}
           {data.length < 1 && (
             <div className="font-semibold flex items-center text-2xl mt-10 h-full w-full">
               <span className="text-center mx-auto font-semibold text-gray-700">
